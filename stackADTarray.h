9758a7e08da9f63817906fae3356228a2fbc85df
Renee-P <143294599+Renee-P@users.noreply.github.com>
2023-09-17 01:34:13 +0800

    Update stackADTarray.h

--- stackADTarray.h
+++ stackADTarray.h
@@ -14,5 +14,31 @@ int isEmpty(STACK S);
 int isFull(STACK S); //1 - true, 0 - false
 void display(STACK S);
 
+void initialize(STACK *S){
+    S->top = MAX-1;
+}
+
+void push(STACK *S,char data){
+    if(isFull(S) == 0){
+        S->top--;
+        S->elem[S->top] = data;
+    } //else if(isFull(S)== 1){}
+}
+
+void pop(STACK *S){
+
+}
+
+int isEmpty(STACK *S){
+    return (S->top == MAX-1) ? 1 : 0; //1 is Empty, 0 is not Empty
+}
+
+int isFull(STACK *S){
+    return (S->top == 0) ? 1 : 0; //1 is Full, 0 is not Full
+}
+
+void display(STACK *S){
+    
+}
 
 #endif
